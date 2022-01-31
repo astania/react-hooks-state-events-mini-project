6083b593acdd3bba001ab7c743405304f326e043
+ <+>
2022-01-31 16:18:15 -0500

    Done.

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import CategoryFilter from "./CategoryFilter";
 import NewTaskForm from "./NewTaskForm";
 import TaskList from "./TaskList";
@@ -8,12 +8,19 @@ console.log("Here's the data you're working with");
 console.log({ CATEGORIES, TASKS });
 
 function App() {
+  const [tasks, setTasks] = useState(TASKS)
+  const [categories, setCategories] = useState(CATEGORIES)
+
+  const addTask = (task) => {
+    setTasks([...tasks, task])
+  }
+
   return (
     <div className="App">
       <h2>My tasks</h2>
-      <CategoryFilter />
-      <NewTaskForm />
-      <TaskList />
+      <CategoryFilter categories={categories} />
+      <NewTaskForm addTask={addTask} />
+      <TaskList tasks={tasks} />
     </div>
   );
 }
